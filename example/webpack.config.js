4812783fbe78534ed544f3cfff3bfaf407aebb68
jaryway <weizengxun@live.cn>
2020-03-10 18:18:28 +0800

    添加 ts 编译支持

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -22,33 +22,75 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.less$/,
-        use: [
-          { loader: "style-loader" },
-          { loader: "css-loader" },
+        oneOf: [
           {
-            loader: "less-loader",
+            test: /\.less$/,
+            exclude: [/\.(js|mjs|jsx|ts|tsx)$/, /\.html$/, /\.json$/],
+            use: [
+              { loader: "style-loader" },
+              { loader: "css-loader" },
+              {
+                loader: "less-loader",
+                options: {
+                  strictMath: true,
+                  noIeCompat: true,
+                  javascriptEnabled: true
+                }
+              }
+            ]
+          },
+          {
+            test: /\.css/,
+            exclude: [/\.(js|mjs|jsx|ts|tsx)$/, /\.html$/, /\.json$/],
+            use: [{ loader: "style-loader" }, { loader: "css-loader" }]
+          },
+          {
+            test: /\.tsx?$/,
+            loader: "babel-loader",
             options: {
-              strictMath: true,
-              noIeCompat: true,
-              javascriptEnabled: true
+              presets: ["@babel/preset-typescript"],
+              plugins: [
+                [
+                  "@babel/plugin-transform-typescript",
+                  { allowNamespaces: true }
+                ]
+              ]
+            }
+          },
+          {
+            test: /\.(js|mjs|jsx|ts|tsx)$/,
+            exclude: /node_modules/,
+            use: {
+              loader: "babel-loader",
+              options: {
+                presets: ["@babel/preset-env", "@babel/preset-react"],
+                cacheDirectory: true
+              }
             }
           }
+          // {
+          //   test: /\.(js|mjs)$/,
+          //   exclude: /@babel(?:\/|\\{1,2})runtime/,
+          //   loader: require.resolve("babel-loader"),
+          //   options: {
+          //     babelrc: false,
+          //     configFile: false,
+          //     compact: false,
+          //     // presets: [
+          //     //   [require.resolve('babel-preset-react-app/dependencies'), { helpers: true }],
+          //     // ],
+          //     cacheDirectory: true,
+          //     // See #6846 for context on why cacheCompression is disabled
+          //     cacheCompression: false,
+
+          //     // If an error happens in a package, it's possible to be
+          //     // because it was compiled. Thus, we don't want the browser
+          //     // debugger to show the original code. Instead, the code
+          //     // being evaluated would be much more helpful.
+          //     sourceMaps: false
+          //   }
+          // }
         ]
-      },
-      {
-        test: /\.css/,
-        use: [{ loader: "style-loader" }, { loader: "css-loader" }]
-      },
-      {
-        test: /\.js/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env", "@babel/preset-react"]
-          }
-        }
       }
     ]
   },
