a482e4c70dd854120fed2eddf2b503281c38d7d6
jaryway <weizengxun@live.cn>
2021-06-08 10:58:35 +0800

    remove unneed file

--- es/MergeableTable.d.ts
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { MergeableTableProps } from "./helper";
-import "rc-dropdown/assets/index.css";
-import "./style";
-declare const _default: React.ForwardRefExoticComponent<MergeableTableProps & React.RefAttributes<any>>;
-export default _default;
--- es/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { default } from "./MergeableTable";
-export { default as Preview } from "./Preview";
-export { BUTTON_CODE, Cell, MOUSE, Element, MergeableTableProps, Range } from "./helper";
